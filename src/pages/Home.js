91692d8b898c45b46722a411778b9efc5bcff3fb
Ygor <108394922+Igor-Kay@users.noreply.github.com>
2023-03-20 05:58:39 +0700

    change tech

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,11 +11,11 @@ const Home = () => {
                     <ul className ="content-list">
                         <li className ="content-list__item">
                             <h2 className ="title-2">Frontend</h2>
-                            <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
+                            <p>JavaScript, TypeScript, ReactJS, HTML, CSS, NPM, Yarn</p>
                         </li>
                         <li className ="content-list__item">
                             <h2 className ="title-2">Backend</h2>
-                            <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+                            <p>NodeJS, MySQL, C++, C# (ASP.NET)</p>
                         </li>
                     </ul>
     
